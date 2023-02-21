58e4c28a460a5f0e0a2e18c09f547f84d0ab1ccd
socry <leonardosbo02@gmail.com>
2023-02-21 12:45:44 -0300

    run webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,19 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
-  mode: "development",
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
       },
     ],
   },
